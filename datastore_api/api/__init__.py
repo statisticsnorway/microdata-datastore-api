a17f31b20af6f86cff1763908bdc4cffc77d4886
DanielElisenberg <Daniel@Elisenberg.com>
2025-07-23 13:30:04 +0200

    add_job_service: job api and tests

--- datastore_api/api/__init__.py
+++ datastore_api/api/__init__.py
@@ -6,6 +6,7 @@ from datastore_api.api.observability import observability_router
 from datastore_api.api import maintenance_status
 from datastore_api.api import targets
 from datastore_api.api import importable_datasets
+from datastore_api.api import jobs
 
 
 def include_routers(app: FastAPI):
@@ -15,3 +16,4 @@ def include_routers(app: FastAPI):
     app.include_router(maintenance_status.router)
     app.include_router(targets.router)
     app.include_router(importable_datasets.router)
+    app.include_router(jobs.router)
