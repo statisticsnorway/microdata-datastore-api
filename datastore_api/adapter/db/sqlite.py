02a188ba5fb412fc201b74ab68a4ad60dd1db572
pawbu <pawbu@users.noreply.github.com>
2025-09-03 15:13:36 +0200

    sqlite: safer sqlite

--- datastore_api/adapter/db/sqlite.py
+++ datastore_api/adapter/db/sqlite.py
@@ -39,6 +39,7 @@ class SqliteDbClient:
         conn = sqlite3.connect(
             self.db_path,
             detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES,
+            isolation_level=None,
         )
         conn.row_factory = sqlite3.Row
         conn.execute("PRAGMA foreign_keys = ON")
