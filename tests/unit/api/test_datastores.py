5e560f33a355c8b4d4b42aae3e854c1ba9a1230e
Daniel Elisenberg <33904479+DanielElisenberg@users.noreply.github.com>
2025-10-27 10:20:53 +0100

    Datastore endpoint (#33)
    * datastore_endpoint: new datastore endpoint
    
    * datastore_endpoint: tests for endpoint
    
    * datastore_endpoint: lint

---
+++ tests/unit/api/test_datastores.py
@@ -0,0 +1,41 @@
+from unittest.mock import Mock
+
+import pytest
+from fastapi.testclient import TestClient
+
+from datastore_api.adapter import db
+from datastore_api.adapter.db.models import Datastore
+from datastore_api.api.common import dependencies
+from datastore_api.main import app
+
+DATASTORE = Datastore(
+    datastore_id=1,
+    rdn="no.dev.test",
+    description="Datastore for testing",
+    directory="tests/resources/test_datastore",
+    name="Test datastore",
+    bump_enabled=False,
+)
+
+
+@pytest.fixture
+def mock_db_client():
+    mock = Mock()
+    mock.get_datastore = Mock(side_effect=lambda datastore_id: DATASTORE)
+    return mock
+
+
+@pytest.fixture
+def client(mock_db_client):
+    app.dependency_overrides[db.get_database_client] = lambda: mock_db_client
+    app.dependency_overrides[dependencies.get_datastore_id] = lambda: 1
+    yield TestClient(app)
+    app.dependency_overrides.clear()
+
+
+def test_get_datastore(client):
+    response = client.get(
+        "/datastores/no.dev.test", headers={"X-Request-ID": "abc123"}
+    )
+    assert response.status_code == 200
+    assert response.json() == DATASTORE.model_dump()
