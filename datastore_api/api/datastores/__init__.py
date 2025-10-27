e15b18580935da2b4bd169e95d1da477d5541412
linnbjornerud <linnchb@gmail.com>
2025-10-27 13:36:09 +0100

    move datastores-router to /datastores

--- datastore_api/api/datastores/__init__.py
+++ datastore_api/api/datastores/__init__.py
@@ -1,8 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
+from datastore_api.adapter import db
+from datastore_api.adapter.db.models import Datastore
+from datastore_api.api.common.dependencies import get_datastore_id
 from datastore_api.api.datastores import jobs
 
 router = APIRouter()
+
+
+@router.get("")
+async def get_datastore(
+    db_client: db.DatabaseClient = Depends(db.get_database_client),
+    datastore_id: int = Depends(get_datastore_id),
+) -> Datastore:
+    return db_client.get_datastore(datastore_id)
 
 
 router.include_router(jobs.router, prefix="/jobs")
