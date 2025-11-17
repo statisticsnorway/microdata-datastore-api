b1490b056be6642b69b55714e5c97d5e9355a11f
DanielElisenberg <Daniel@Elisenberg.com>
2025-11-17 11:27:20 +0100

    job_endpoint_for_datastores: lint

--- datastore_api/api/datastores/jobs/__init__.py
+++ datastore_api/api/datastores/jobs/__init__.py
@@ -50,7 +50,7 @@ def get_job(
     auth_client: auth.AuthClient = Depends(auth.get_auth_client),
     database_client: db.DatabaseClient = Depends(db.get_database_client),
 ) -> Job:
-    parsed_user_info = auth_client.authorize_data_administrator(
+    auth_client.authorize_data_administrator(
         authorization, user_info
     )
     job = database_client.get_job(job_id)
