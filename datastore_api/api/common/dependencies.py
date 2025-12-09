23e6b212ec3924b73427c0499f239e1d7664947f
Jon Haraldsvik <haraldsvik91@gmail.com>
2025-12-09 13:02:44 +0100

    get rdn from request directly

--- datastore_api/api/common/dependencies.py
+++ datastore_api/api/common/dependencies.py
@@ -1,36 +1,23 @@
 import logging
 from pathlib import Path
 
-from fastapi import Depends, Request
+from fastapi import Depends
 
 from datastore_api.adapter import db
 from datastore_api.common.exceptions import (
     DatastoreNotFoundException,
-    DatastoreRdnMissingException,
 )
 
 logger = logging.getLogger()
 
 
-def get_datastore_rdn_from_request(request: Request) -> str:
-    datastore_rdn: str | None = request.path_params.get("datastore_rdn")
-
-    if datastore_rdn is None:
-        raise DatastoreRdnMissingException(
-            "RDN is required but not found in request path"
-        )
-
-    return datastore_rdn
-
-
 def get_datastore_id(
-    request: Request,
+    datastore_rdn: str,
     database_client: db.DatabaseClient = Depends(db.get_database_client),
 ) -> int:
     """Returns the datastore ID corresponding to the RDN in the request path.
     If no RDN is present it raises an error."""
 
-    datastore_rdn = get_datastore_rdn_from_request(request)
     datastore_id = database_client.get_datastore_id_from_rdn(datastore_rdn)
 
     if datastore_id is None:
