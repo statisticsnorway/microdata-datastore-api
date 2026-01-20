f7e29780f5bdedc1c223a202ef2041c25e81fafc
Johannes <jfi@ssb.no>
2026-01-20 14:02:44 +0100

    Adding tests

--- tests/integration/adapter/test_input_directory.py
+++ tests/integration/adapter/test_input_directory.py
@@ -1,11 +1,20 @@
+import os
+import shutil
 from pathlib import Path
+
+import pytest
 
 from datastore_api.adapter.local_storage import input_directory
 from datastore_api.adapter.local_storage.input_directory import (
     ImportableDataset,
 )
+from datastore_api.common.exceptions import (
+    NameValidationError,
+    NotFoundException,
+)
 
 INPUT_DIR = Path("tests/resources/test_datastore_input")
+
 
 expected_datasets = [
     ImportableDataset(
@@ -44,3 +53,25 @@ def test_get_importable_datasets_filter():
 
     for dataset in actual_datasets:
         assert dataset in my_expected_datasets
+
+
+def test_create_and_delete_importable_dataset():
+    shutil.copyfile(
+        f"{INPUT_DIR}/MY_DATASET.tar",
+        f"{INPUT_DIR}/DATASET_FOR_DELETE.tar",
+    )
+    input_directory.delete_importable_datasets("DATASET_FOR_DELETE", INPUT_DIR)
+
+    assert "DATASET_FOR_DELETE.tar" not in os.listdir(INPUT_DIR)
+
+
+def test_delete_importable_datasets_raises_not_found_exception():
+    with pytest.raises(NotFoundException):
+        input_directory.delete_importable_datasets("NO_SUCH_DATASET", INPUT_DIR)
+
+
+def test_delete_importable_datasets_raises_name_validation_error():
+    with pytest.raises(NameValidationError):
+        input_directory.delete_importable_datasets(
+            "1_INVALID_DATASET_NAME", INPUT_DIR
+        )
