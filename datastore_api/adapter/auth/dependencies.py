0a5594ed113d9e493e85bd1c472ec2d14699a7e4
Linn Bjørnerud <125447659+linnbjornerud@users.noreply.github.com>
2026-02-02 14:19:50 +0100

    Update order
    Co-authored-by: Daniel Elisenberg <33904479+DanielElisenberg@users.noreply.github.com>

--- datastore_api/adapter/auth/dependencies.py
+++ datastore_api/adapter/auth/dependencies.py
@@ -4,14 +4,6 @@ from datastore_api.adapter import auth
 from datastore_api.adapter.db.models import UserInfo
 
 
-def require_data_administrator(
-    datastore_rdn: str,
-    authorization: str | None = Cookie(None),
-    auth_client: auth.AuthClient = Depends(auth.get_auth_client),
-) -> None:
-    auth_client.authorize_data_administrator(datastore_rdn, authorization)
-
-
 def require_datastore_provisioner(
     authorization: str | None = Cookie(None),
     auth_client: auth.AuthClient = Depends(auth.get_auth_client),
@@ -24,6 +16,16 @@ def require_api_key(
     api_key: str = Header("X-API-KEY", alias="x-api-key"),
 ) -> None:
     auth_client.check_api_key(api_key)
+
+
+def require_data_administrator(
+    datastore_rdn: str,
+    authorization: str | None = Cookie(None),
+    auth_client: auth.AuthClient = Depends(auth.get_auth_client),
+) -> None:
+    auth_client.authorize_data_administrator(datastore_rdn, authorization)
+
+
 
 
 def require_data_administrator_with_user_info(
